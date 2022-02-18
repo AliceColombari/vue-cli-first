458b978acf6ce2c964a8eeb3572be71f097a9297
AliceColombari <colombari.23@gmail.com>
2022-02-18 14:20:20 +0100

    creato un nuovo file.vue e aggiunto h1 e proprietà

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <NewTitle></NewTitle>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NewTitle from './components/NewTitle.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NewTitle
   }
 }
 </script>
---
+++ src/components/NewTitle.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>La mia prima app con Vue CLI!</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+  name: 'NewTitle'
+}
+</script>
+
+<style scoped>
+    h1 {
+        color: #007f00;
+    }
+</style>
